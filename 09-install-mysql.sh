c2f458d55d33aed1dfeb02da51567ba80c3662f3
shaikmadeen <shaikmadeen29@gmail.com>
2024-04-30 20:04:07 +0530

    shell practice

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -9,3 +9,10 @@ then
 #     echo "INFO:: you are root user"
 fi
 yum install mysql -y 
+ if [ $? -ne 0 ]
+ then
+     echo "Installation of mysql is error"
+     exit 1
+else
+    echo "Installation of mysql is success"
+fi        
