31d8143220b4a6d6dc681f646981c11a1872f1e1
shaikmadeen <shaikmadeen29@gmail.com>
2024-04-30 14:21:38 +0530

    third commit

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Madeen
+PEESON2=Gopi
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, very good morning'
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: Iam fine $PERSON1, and what about you?"
