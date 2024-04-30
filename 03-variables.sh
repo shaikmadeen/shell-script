10c769b02cfd64052ff6950732b8375a2d36df5f
shaikmadeen <163907894+shaikmadeen@users.noreply.github.com>
2024-04-30 14:45:32 +0530

    third commit

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,8 @@
 PERSON1=Madeen
 PEESON2=Gopi
 
+
 echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, very good morning'
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: Iam fine $PERSON1, and what about you?"
+echo "$PERSON2: I am good $PERSON1, How are you"
