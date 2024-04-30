001e8f15b0e9a2f489f8d110ede57e99a64271f0
shaikmadeen <shaikmadeen29@gmail.com>
2024-04-30 19:49:25 +0530

    shell practice

---
+++ 09-install-mysql.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# our program goal is to install mysql
+USERID=$(id -u)
+if [ $USERID -ne 0]
+then
+    echo "ERROR:: please run this script with root access"
+# else
+#     echo "INFO:: you are root user"
+fi
+yum install mysql -y 
