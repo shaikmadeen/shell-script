aae0c61b848e1633d85317c019dad14b3042ce0b
shaikmadeen <shaikmadeen29@gmail.com>
2024-04-30 20:10:49 +0530

    shell practice

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # our program goal is to install mysql
 USERID=$(id -u)
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "ERROR:: please run this script with root access"
     exit 1
@@ -15,4 +15,14 @@ yum install mysql -y
      exit 1
 else
     echo "Installation of mysql is success"
-fi        
+fi
+
+yum install postfix -y
+ 
+if [ $? -ne 0 ]
+then
+    echo "Installation of postfix is error"
+    exit 1
+else
+    echo "Installation of postfix is success"
+fi         
