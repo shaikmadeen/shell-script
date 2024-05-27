8f6b3fdd3417b8240adfeffd9a937357d7fe4703
shaikmadeen <shaikmadeen29@gmail.com>
2024-05-28 01:03:47 +0530

    shell script

--- 13.loops.sh
+++ 13.loops.sh
@@ -16,14 +16,23 @@ then
    exit 1
 fi 
 
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then 
+       echo -e "Installing $2 ... $R FAILURE $N"
+       exit 1
+    else
+       echo -e "Installing $2 ... $G SUCCESS $N"
+}
 # all args are in $@
 for i in $@
 do
-   yum list installesd $i 
+   yum list installesd $i &>>$LOGFILE
    if [ $? -ne 0 ]
    then 
        echo "$i is not installed, let's install it"
-       yum install $i -y 
+       yum install $i -y &>>$LOGFILE
+       VALIDATE $? "$i"
    else
        echo -e "$Y $i is already installed $N"
    fi 
